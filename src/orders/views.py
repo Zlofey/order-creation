46e5ab6477956834adc8685b80cca7710ef06f44
Tim <tatarovtk@gmail.com>
2026-03-21 17:55:30 +0300

    create base view structure

--- src/orders/views.py
+++ src/orders/views.py
@@ -1,7 +1,18 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from orders.models import Order
+from orders.serializers import OrderSerializer
+
+# class OrderCreateView(CreateAPIView):
+#     queryset = Order.objects.all()
 
 
-class OrderCreateView(CreateAPIView):
-    queryset = Order.objects.all()
+class OrderCreateView(APIView):
+    """
+    Заглушка для будущей View по созданию заказа.
+    """
+
+    def post(self, request):
+        serializer = OrderSerializer(data=request.data)
+        serializer.is_valid()
+        return Response(serializer.data)
